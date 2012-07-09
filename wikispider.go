0837c4c035dd43cd0aca77a005d9ece4b30dc406
gaedol <marco@jackson>
2012-07-09 14:13:48 -0400

    2 separate files, added lots of stuff: handling redirects and santizing links and infoboxes

--- wikispider.go
+++ wikispider.go
@@ -1,89 +1,24 @@
 package main
 
+
 import (
+	"strings"
 	"net"
 	"net/url"
 	"net/http"
 	"log"
-	"io/ioutil"
+	//	"io/ioutil"
 	"os"
 	"flag"
 	"path/filepath"
 	"sync"
 	"time"
+	"fmt" 
 )
 
-type Article struct {
-	title string
-	body []byte
-}
-
-func (art *Article) Download(cachePath string, client http.Client, limiter chan bool) ( cached, ok bool ) {
-
-	if art.body != nil {
-		panic("Article already loaded")
-	}
-
-	escaped := url.QueryEscape(art.title)
-	cachePath = filepath.Join(cachePath, escaped + ".wiki")
-	_, err := os.Stat(cachePath)
-
-	if err == nil {
-		file, _ := os.Open(cachePath)
-		art.body, _ = ioutil.ReadAll(file)
-		file.Close()
-		return true, true
-	}
-	
-	<-limiter
-
-	url := "http://en.wikipedia.org/w/index.php?title=" + escaped + "&action=raw"
-
-	resp, err := client.Get(url)
-	if err != nil {
-		return false, false
-	}
-
-	defer resp.Body.Close()
-	art.body, err = ioutil.ReadAll(resp.Body)
-
-	if err != nil {
-		return false, false
-	}
-
-	file, err := os.Create(cachePath)
-	if err == nil { 
-		file.Write(art.body)
-		file.Close()
-	} else {
-		log.Printf("Couldn't write body of page %s to disk", art.title)
-		return false, false
-	}
-
-	return false, true
-}
-
-// return all non-template internal wikipedia links in a given article 
-func (a* Article) Links() []string {
-	slices := make([]string, 0, 32)
-
-outer: for s := a.body ; len(s) > 0 ; s = s[1:] {
-		if s[0] == '[' && s[1] == '[' {
-			var i int
-			for i = 0; s[i] != ']' && s[i] != '|' ; i++ {
-				if s[i] == ':' {
-					continue outer
-				}
-			}
-			slices = append(slices, string(s[2:i]))
-		}
-	}
-
-	return slices
-}
 
 func RateLimiter(delay int) chan bool {
-	
+
 	limiter := make(chan bool)
 	go func() {
 		if delay == 0 {
@@ -101,7 +36,7 @@ func RateLimiter(delay int) chan bool {
 	return limiter
 }
 
-func Spider(titles []string, path string, maxdepth, maxwidth, pool, delay int) {
+func Spider(titles []string, path string, maxdepth, maxwidth, pool, delay int, kind string) {
 
 	var total, total_new, total_errors int
 
@@ -110,10 +45,10 @@ func Spider(titles []string, path string, maxdepth, maxwidth, pool, delay int) {
 
 	// rate limiter, shared by all download workers
 	limiter := RateLimiter(delay)
-	
+
 	// how many articles are currently downloading, used as a barrier to next generation
 	counter := new(sync.WaitGroup)
-	
+
 	// which articles have we already visited
 	visited := make(map[string]bool, 8192)
 
@@ -128,8 +63,8 @@ func Spider(titles []string, path string, maxdepth, maxwidth, pool, delay int) {
 		for {
 			if len(visitBuffer) == 0 { visitBuffer = append(visitBuffer, <-visitWriter) } 
 			select {
-			case visitReader <- visitBuffer[0]: visitBuffer = visitBuffer[1:]; 
-			case page := <- visitWriter: visitBuffer = append(visitBuffer, page); 
+				case visitReader <- visitBuffer[0]: visitBuffer = visitBuffer[1:]; 
+				case page := <- visitWriter: visitBuffer = append(visitBuffer, page); 
 			}
 		} 
 	}()
@@ -141,117 +76,148 @@ func Spider(titles []string, path string, maxdepth, maxwidth, pool, delay int) {
 			// timeout code modified from https://groups.google.com/group/golang-nuts/browse_thread/thread/d9e86a6fab79e240
 			client := http.Client{Transport: &http.Transport{
 				Dial: func(netw, addr string) (net.Conn, error) { 
-						c, err := net.DialTimeout(netw, addr, time.Duration(2e9))
-						if err != nil { return nil, err } 
-						return c, nil
-					},
+					c, err := net.DialTimeout(netw, addr, time.Duration(2e9))
+					if err != nil { return nil, err } 
+					return c, nil
 				},
-			}
-			for page := range download {
-				
-				cached, ok := page.Download(path, client, limiter)
-				
-				if ok {
-					total++
-					if !cached {
-						log.Printf("\tDownloaded \"%s\"", page.title)
-						total_new++
-					} else {
-						log.Printf("\tAlready have \"%s\"", page.title)
-					}
-					visitWriter <- page	
+			},
+		}
+		for page := range download {
+
+			cached, ok := page.Download(path, client, limiter)
+
+			if ok {
+				total++
+				if !cached {
+					log.Printf("\tDownloaded \"%s\"", page.title)
+					total_new++
 				} else {
-					log.Printf("\tError downloading \"%s\"", page.title)
-					total_errors++
+					log.Printf("\tAlready have \"%s\"", page.title)
 				}
-				
-				counter.Done()
+				visitWriter <- page	
+			} else {
+				log.Printf("\tError dowloading \"%s\"", page.title)
+				total_errors++
 			}
-		}()
+
+			counter.Done()
+		}
+	}()
+}
+
+// kickstart the first generation
+// nil signals the end of a generation; the first generation is entirely empty
+depth := 0
+visitWriter <- nil 
+for _, t := range titles {
+	t=SanitizeTitle(t)
+	counter.Add(1)
+	download <- &Article{title:t}
+}
+
+// loop through the visit queue, which consists of the downloaded pages in breadth-first order
+// successive depths ('generations') are separated by 'nil' pages
+for page := range visitReader {
+
+	if page != nil && depth <= maxdepth { // did we get a downloaded page to process?
+
+		links := page.Links()
+		if kind != "" && !Intersect(page.infobox, kind) {
+			continue
+		}
+		width := 0
+
+		graph[page.title] = links
+
+		for _, link := range links {
+
+			// articles are cached to disk, but this allows us to avoid
+			// counting links we've already seen towards the width limit
+			if visited[link] { continue } 
+			visited[link] = true
+			//should add some logic to do this: IF art.redirects > 0, then do not
+			//Sanitize, else, sanitize. How do I access art.redirects?
+			//DONE in the Download() Function
+
+			counter.Add(1)
+			download <- &Article{title:link}
+
+			width++
+			if width > maxwidth { break }
+		}
+
+	} else { // did we hit this generation's cap?
+
+		if depth <= maxdepth {
+			log.Printf("Downloading generation %d\n", depth)
+		}
+
+		// wait for the next generation's visits are all on the queue by 
+		// waiting for the current generation to finish downloading
+		counter.Wait()
+
+		if depth > maxdepth { break }
+		depth++
+
+		// cap the next generation
+		visitWriter <- nil
 	}
+}
 
-	// kickstart the first generation
-	// nil signals the end of a generation; the first generation is entirely empty
-	depth := 0
-	visitWriter <- nil 
-	for _, t := range titles {
-		counter.Add(1)
-		download <- &Article{title:t}
+// write the graph
+//FIXME: if we have more terms, we now write just one graph. We should instead
+//draw two different ones.
+graphfilename := fmt.Sprintf("graph_%s_%d_%d.tsv", strings.Join(titles,"_"), maxdepth, maxwidth)
+graphpath := filepath.Join(path,graphfilename)
+file, err := os.Create(graphpath)
+if err == nil {
+	for title, links := range graph {
+		str := url.QueryEscape(title)
+		for _, link := range links { str += "\t" + url.QueryEscape(link) }
+		str += "\n"
+		file.Write([]byte(str))
 	}
+} else {
+	log.Printf("Couldn't write graph to disk, err is %s", graphpath,err)
+}
 
-	// loop through the visit queue, which consists of the downloaded pages in breadth-first order
-	// successive depths ('generations') are separated by 'nil' pages
-	for page := range visitReader {
+// TODO: Properly clean up infinite channel, download workers
 
-		if page != nil && depth <= maxdepth { // did we get a downloaded page to process?
+log.Printf("Visited %d unique pages, downloaded %d pages, %d errors", total, total_new, total_errors)
+file.Close()
 
-			links := page.Links()
-			width := 0
+}
 
-			graph[page.title] = links
-			
-			for _, link := range links {
+func SanitizeTitle( oldtitle string) (newtitle string) {
 
-				// articles are cached to disk, but this allows us to avoid
-				// counting links we've already seen towards the width limit
-				if visited[link] { continue } 
-				visited[link] = true
-				
-				counter.Add(1)
-				download <- &Article{title:link}
-				
-				width++
-				if width > maxwidth { break }
-			}
+	return strings.Replace(strings.ToUpper(string(oldtitle[0])) + strings.ToLower(oldtitle[1:]), " ", "_", -1)
 
-		} else { // did we hit this generation's cap?
+}
 
-			if depth <= maxdepth {
-				log.Printf("Downloading generation %d\n", depth)
-			}
-			
-			// wait for the next generation's visits are all on the queue by 
-			// waiting for the current generation to finish downloading
-			counter.Wait()
-
-			if depth > maxdepth { break }
-			depth++
-
-			// cap the next generation
-			visitWriter <- nil
+func Intersect(infoboxes []string , kind string ) (ok bool) {
+	var hits int
+	for _,v := range(infoboxes){
+		if v == kind{
+			hits++
 		}
 	}
-
-	// write the graph
-	graphpath := filepath.Join(path, "graph.tsv")
-	file, err := os.Create(graphpath)
-	if err == nil {
-		for title, links := range graph {
-			str := url.QueryEscape(title)
-			for _, link := range links { str += "\t" + url.QueryEscape(link) }
-			str += "\n"
-			file.Write([]byte(str))
-		}
-		file.Close()
-	} else {
-		log.Printf("Couldn't write graph to disk", graphpath)
-	}
-
-	// TODO: Properly clean up infinite channel, download workers
-	
-	log.Printf("Visited %d unique pages, downloaded %d pages, %d errors", total, total_new, total_errors)
+	if hits>0{
+		return true
+	} 
+	return false
 }
 
 func main() {
 
 	var depth, width, pool, limit int
-	var path string
-	
-	flag.IntVar(&depth, "depth", 1, "Depth to traverse to")
+	var path,kind string
+
+	flag.IntVar(&depth, "depth", 2, "Depth to traverse to")
 	flag.IntVar(&width, "width", 3, "Number of links to get from each page")
 	flag.IntVar(&pool, "pool", 4, "Number of simultaneous downloads")
 	flag.IntVar(&limit, "limit", 300, "Delay between downloads, in milliseconds (global to pool)")
 	flag.StringVar(&path, "path", "pages", "Directory in which to put the visited pages")
+	flag.StringVar(&kind, "kind", "", "Which kind of infoboxes we want to download")
 	flag.Parse()
 
 	if flag.NArg() == 0 {
@@ -269,6 +235,5 @@ func main() {
 	} else if !p.IsDir() {
 		panic("Output path is not a directory")
 	}
-
-	Spider(flag.Args(), path, depth, width, pool, limit)
+	Spider(flag.Args(), path, depth, width, pool, limit, kind)
 }
